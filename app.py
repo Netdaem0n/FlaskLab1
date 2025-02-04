7def3acc386e027b03c40e800c96162b9b15a3d4
Netdaemon <bakalets@gmail.com>
2025-02-04 16:31:36 +1100

    Test case 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! I m BACK!!!!'
+    return 'Привет Мир. Это CI/CD!'
 
 
 if __name__ == '__main__':
--- runner_script.sh
+++ runner_script.sh
@@ -4,7 +4,7 @@
 echo "ОК"
 HOME_DIR="$HOME"
 REPORT_FILE="$HOME_DIR/report.txt"
-CURRENT_DATE=$(date 'Дата: +%d-%m-%Y Время: %H:%M:%S')
+CURRENT_DATE=$(date '+%d-%m-%Y %H:%M:%S')
 echo "ОК - $CURRENT_DATE" >> "$REPORT_FILE"
 echo "Файл отчета создан: $REPORT_FILE"
 chmod 644 "$REPORT_FILE"
