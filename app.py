8b4717520bf664d26dbe3d0ea03263f66a68da96
Netdaemon <bakalets@gmail.com>
2025-02-04 16:36:19 +1100

    Test case 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Привет Мир. Это CI/CD!'
+    return 'Привет Мир. Это CI/CD! вариант 2.'
 
 
 if __name__ == '__main__':
