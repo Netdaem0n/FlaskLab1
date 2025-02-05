d1192678da2ecc9191c5618450815aa7dc87ec89
Netdaemon <bakalets@gmail.com>
2025-02-05 13:59:34 +1100

    Test case 2

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, jsonify, render_template, request, url_for
 
 app = Flask(__name__)
-
+ver = "0.2alfa"  # Определяем ver здесь!
+app.config['APPLICATION_ROOT'] = '/lab1/'
 
 @app.route('/')
 def index():
     data = request.remote_addr
-    return render_template('index.html', **{'ver': '0.1',
+    return render_template('index.html', **{'ver': ver,
                                            'data': data})
 
 @app.route('/about')
 def about():
     data = request.remote_addr
-    return render_template('about.html', **{'ver': '0.2',
+    return render_template('about.html', **{'ver': ver,
                                            'data': data})
 
 @app.route('/test')
 def test():
     data = request.remote_addr
-    return render_template('test.html', **{'ver': '0.1',
+    return render_template('test.html', **{'ver': ver,
                                            'data': data})
 
 if __name__ == '__main__':
-    app.config['APPLICATION_ROOT'] = '/lab1'
     app.run()
